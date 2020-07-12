3e6a973e11c96cf334b533dfae493d83608ddc62
Waqar Ahmed <waqar.17a@gmail.com>
2020-07-12 17:02:58 +0500

    Fix regex in demangler

--- asmparser.cpp
+++ asmparser.cpp
@@ -2,6 +2,7 @@
 
 #include <QTextStream>
 #include <QDebug>
+#include <QRegularExpression>
 
 #include <cxxabi.h>
 
@@ -23,28 +24,39 @@ QString AsmParser::process(const QByteArray &asmText)
     int LFEcount = 0;
     bool isLFEBlock = false;
 
+    //1. collect all the labels
+    //2. go through the asm line by line and check if the labels are used/unused
+    //3. if the labels are unused, they get deleted
+    //4. every line beginning with '.' gets deleted, unless it is a used label
+
+    QRegularExpression exp{"^\\s*[_|a-zA-Z]"};
+
     while (!s.atEnd()) {
         QString line = s.readLine();
-        bool isLFBDirective = line.startsWith(QLatin1String(".LFB"));
-        bool isLFEDirective = line.startsWith(QLatin1String(".LFE"));
-
-        if (isLFEDirective) {
-            isLFEBlock = true;
-        }
-
-        if (isLFEBlock) {
-            LFEcount++;
-            if (LFEcount == 4) {
-                isLFEBlock = false;
-            }
-            continue;
-        }
-
-        if (!isLFBDirective && !isLFEDirective && !isLFEBlock) {
-            if (line.startsWith(".LFB"))
-                qDebug () << line;
+//        auto match = exp.match(line);
+//        if (match.hasMatch()) {
             output.append(line + "\n");
-        }
+//        }
+//        bool isLFBDirective = line.startsWith(QLatin1String(".LFB"));
+//        bool isLFEDirective = line.startsWith(QLatin1String(".LFE"));
+
+//        if (isLFEDirective) {
+//            isLFEBlock = true;
+//        }
+
+//        if (isLFEBlock) {
+//            LFEcount++;
+//            if (LFEcount == 4) {
+//                isLFEBlock = false;
+//            }
+//            continue;
+//        }
+
+//        if (!isLFBDirective && !isLFEDirective && !isLFEBlock) {
+//            if (line.startsWith(".LFB"))
+//                qDebug () << line;
+//            output.append(line + "\n");
+//        }
     }
 
     return output;
@@ -58,13 +70,14 @@ QString AsmParser::demangle(QString &&asmText)
         next = asmText.indexOf("_Z", last);
         //get token
         if (next != -1) {
-            int token = asmText.indexOf(QRegExp(":|,|@|\\[\\s|\\n"), next + 1);
+            int token = asmText.indexOf(QRegularExpression(":|,|@|\\[|\\s|\\n"), next + 1);
             int len = token - next;
             QStringRef tok = asmText.midRef(next, len);
             int status = 0;
             char* name = abi::__cxa_demangle(tok.toUtf8().constData(), 0, 0, &status);
             if (status != 0) {
                 qDebug () << "Demangling of: " << tok << " failed, status: " << status;
+                continue;
             }
             QString qName{name};
             qName.prepend(' ');
